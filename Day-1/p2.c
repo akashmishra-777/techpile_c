2e933c530dbc07cc6fd81d7217c4ba93055415e4
akashmishra-777 <leo.akashmishra@gmail.com>
2024-08-18 13:27:03 +0530

    Update p2.c

--- Day-1/p2.c
+++ Day-1/p2.c
@@ -1,12 +1,8 @@
 #include<stdio.h>
-#include<conio.h>
-
 
 int main(){
-	int number = 0;
-	printf("Enter a number : ");
-	scanf("%d",&number);
-	
-	printf("After converting a number to a string : %c ",number);
+	int x = 65;
+	char character = (char) x;
+	printf("%c",character);
 	return 0;
 }
