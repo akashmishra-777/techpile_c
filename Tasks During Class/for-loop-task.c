a05ae9357ddf346f84dee81512335667157fed16
Akash Mishra <leo.akashmishra@gmail.com>
2024-08-21 09:24:08 +0530

    class task of for loop commited

---
+++ Tasks During Class/for-loop-task.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+	
+	for(int i=7; i<=10; i++){
+		printf("Techpile\n");
+	}
+	//1
+	printf("\n");
+	for(int i=18; i>=10; i=i-2){
+		printf("Techpile\n");
+	}
+	//2
+	printf("\n");
+	for(int i=5; i<=20; i=i+5){
+		printf("Techpile\n");
+	}
+	//3
+	printf("\n");
+	for(int i=2; i<=20; i=i+2){
+		printf(":Techpile\n");
+	}
+	//4
+	printf("\n");
+	for(int i=2; i<=5; i++){
+		printf("Techpile\n");
+	}
+	//5
+	
+	printf("\n");
+	for(int i=5; i<=7; i++){
+		printf("Techpile\n");
+	}
+		//6
+	printf("\n");
+	for(int i=28; i>=20; i=i-2){
+		printf("::Techpile\n");
+	}
+
+	//7
+	printf("\n");
+	for(int i=4; i<=8; i=i+2){
+		printf("::Techpile\n");
+	}
+	
+	//8 
+	
+	printf("\n");
+	for(int i=5; i>=1; i=i-1){
+		printf("::Techpile\n");
+	}
+	
+	//9
+	
+	printf("\n");
+	for(int i=5; i>=1; i=i-2){
+		printf("::Techpile\n");
+	}
+	
+	
+	//10
+	
+	printf("\n");
+	for(int i=3; i<=30; i=i+3){
+		printf(":::Techpile\n");
+	}
+	
+	return 0;
+}
+
+
+// initialization i=6;
+// initialization i = 10;
+//initialization i =20;
+// initialization i = 0;
+// condition a<=5
+//condition a<=7
